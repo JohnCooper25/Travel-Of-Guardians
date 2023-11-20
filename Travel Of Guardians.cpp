1f6d02f531e1cb370b18512362403b5b0ac5c1d4
Jonathan Catalan <jluiscatalan1@gmail.com>
2023-11-20 20:44:16 -0300

    third commit
    Implementacion de grafo y lista de adyacencia para grafo

--- Travel Of Guardians.cpp
+++ Travel Of Guardians.cpp
@@ -1,22 +1,89 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
 #include <vector>
+#include <string>
 using namespace std;
 
+//Declaracion de estructura de guardian con los datos que se necesitan para ser cargados desde el archivo
 struct Guardian
 {
 	string Nombre;
 	int Poder;
 	string Maestro;
-	vector<Guardian*> aprendices;
+	string Ciudad;
+	
 };
 
-//Funcion para arbol Binario.
+struct Ciudad {
+    char Nombre[80];
+    char Conexion[50];
+};
+
+struct NodoCiudad {
+    struct Ciudad ciudad;
+    struct NodoCiudad* siguiente;
+};
+
+//************Fin declaracion de estructuras********
+
+/*****Funcionamiento de jerarquia guardianes.*****/
+
+using Arbol = unordered_map<string, vector<Guardian>>;//Codigo para generacion del mapa del arbol en base a un vector.
+
+//En esta funcion se realiza la carga de informacion desde el archivo Guardianes.txt, para luego asignar los datos de todos y cada uno de los guardianes
+void CargaInformacion(const string &Archivocarga, Arbol &arbol) {
+    ifstream Archivo(Archivocarga);
+
+    if (!Archivo.is_open()) {
+        cout << "Error al abrir el archivo '" << Archivocarga << "'.";
+        return;
+    }
+
+    string linea;
+    while (getline(Archivo, linea)) {
+        istringstream ss(linea);
+        Guardian guardian;
+        getline(ss, guardian.Nombre, ',');
+        ss >> guardian.Poder;
+        ss.ignore(); // Ignorar la coma despues del poder
+        getline(ss, guardian.Maestro, ',');
+        getline(ss, guardian.Ciudad);
+
+        // Agregar el guardian al arbol
+        arbol[guardian.Maestro].push_back(guardian);
+    }
+
+    Archivo.close();
+}
+
+//En esta funcion se recorre el arbol creado a partir de los datos del archivo y luego se procede a imprimirlo en el formato que se necesite.
+//En este caso se busca imprimirlos en orden jerarquico.
+void imprimirArbol(const Arbol &arbol, const string &raiz, int nivel) {
+    if (arbol.find(raiz) == arbol.end()) {
+        return;
+    }
+
+    for (const auto &guardian : arbol.at(raiz)) {
+        for (int i = 0; i < nivel; ++i) {
+            cout << "  ";
+        }
+        cout << "- " << guardian.Nombre << " (" << guardian.Ciudad << ")\n";
+
+        // Llamada recursiva para imprimir los hijos del guardian actual
+        imprimirArbol(arbol, guardian.Nombre, nivel + 1);
+    }
+}
+//***************Fin funcionalidades Jerarquia**********
+
+//***********Funcionalidades arbol binario**********
 /*Por medio de esta funcion ordenaremos segun el nivel de poder los guardianes.*/
 struct Ranking
 {
 	int data;
-	Node* left;
-	Node* right;	
+	Ranking* left;
+	Ranking* right;	
 };
 
 //Funcion para crear nodos en el ranking.
@@ -28,6 +95,62 @@ Ranking* CrearRank(int data)
 	Nuevorank -> right = NULL;
 	return Nuevorank;
 }
+
+//*********Fin funcionalidades arbol Binario.********
+
+//**********Funcionalidades grafo Ciudades********
+
+
+//*******Carga de info hacia lista de ciudades********
+void CargainfoCiudades(NodoCiudad*& listaCiudades) {
+    FILE* file = fopen("Ciudades.txt", "r");
+
+    if (file == NULL) {
+        perror("No existe archivo .txt");
+        return;
+    }
+
+    struct NodoCiudad* iterador = NULL;
+
+    // Asignacion de variables a la lista
+    int iterar = 0; // Inicializamos a 0
+
+    while (iterar < 60) {
+        struct NodoCiudad* nuevoNodo = new NodoCiudad; 
+        if (fscanf(file, "%79[^,], %49[^\n]\n",
+                   nuevoNodo->ciudad.Nombre,
+                   nuevoNodo->ciudad.Conexion) == 2) {
+            nuevoNodo->siguiente = NULL;
+            if (listaCiudades == NULL) {
+                listaCiudades = nuevoNodo;
+                iterador = listaCiudades; // Iterador apunta al primer nodo
+            } else {
+                iterador->siguiente = nuevoNodo;
+                iterador = iterador->siguiente; // El nuevo nodo es ahora el ultimo elemento
+            }
+
+            iterar++;
+        } else {
+            break;
+        }
+    }
+
+    fclose(file);
+}
+
+void printCiudades(const NodoCiudad* listaCiudades) {
+    const NodoCiudad* current = listaCiudades;
+    while (current != NULL) {
+        cout << "Ciudad: " << current->ciudad.Nombre << ", Conexion: " << current->ciudad.Conexion << endl;
+        current = current->siguiente;
+    }
+}
+
+//******Fin funcionalidades Lista Ciudades*******
+
+
+//*******Funcionalidades grafo para matriz*******
+
 
 
 // Funcion para agregar ejes y crear grafo de las ciudades
@@ -49,35 +172,37 @@ void printGraph(const vector<vector<int>>& graph) {
 }
 
 
-
-void CargaInformacion()
-{
-			
-}
-
 int main()
 {
 	int menu;
+	
+	Arbol arbol;
+	
+	struct NodoCiudad *listaCiudades = NULL;
+	
 	
 	do
 	{
 		cout << "Hola, Escoja la accion que desea realizar: "<<endl;
 	
-		cout<< "(1)Carga de informacion.\n(2)Ver Lista de Candidatos.\n(3)Ver al Guardian.\n(4)Conocer el reino.\n(5)Presenciar una batalla.\n(0)Salir"<<endl;
+		cout<< "(1)Carga de informacion Guardianes(jerarquia).\n(2)Ver Lista de Candidatos.\n(3)Ver al Guardian.\n(4)Conocer el reino.\n(5)Presenciar una batalla.\n(0)Salir"<<endl;
 		cin>> menu;
 		
 		switch(menu)
 		{
 			case 1:
-				cout<<"Hola caso 1 aweonao.";
+				CargaInformacion("Guardianes.txt", arbol);
+				cout << "arbol de Personajes:\n";
+    			imprimirArbol(arbol, "None", 0);
 				break;
 				
 			case 2: 
-				cout<<"Caso 2 pedazo de pete.";
+				CargainfoCiudades(listaCiudades);
+    			printCiudades(listaCiudades);
 				break;
 			
 			case 3:
-				cout<<"Ahora el 3 aweonaito.";
+				cout<<"Hola";
 				break;
 			
 			case 4: 
